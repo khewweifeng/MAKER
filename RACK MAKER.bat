5ee2b2e43fbd78e7fc11c01efefd1f82ab7716fb
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-07-07 13:29:19 +0800

    Add files via upload

--- RACK MAKER.bat
+++ RACK MAKER.bat
@@ -7,7 +7,7 @@ set /p c= "A OR B: "
 set /p n= "COLUMN NUMBER: "
 
 if "%c%" == "a" / "%c%" == "b" (
-  echo rack%r%-%c%%n% | clip
+  echo rack%r%-%c%0%n% | clip
 ) else (
   echo Invalid input! Try again.
   goto start
