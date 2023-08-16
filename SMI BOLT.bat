075ddcda1cebaae0bd8d2010b729c3ba6e2fd76a
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-08-16 09:46:41 +0800

    Add files via upload

--- SMI BOLT.bat
+++ SMI BOLT.bat
@@ -2,9 +2,13 @@ color 1f
 @ECHO OFF
 :start
 cls
+set /p sm="SM: "
 set /p s= "SMI: "
 
 echo https://bolt.dell.com/_layouts/15/FormServer.aspx?XmlLocation=/ServiceMgmt/%s%.xml
 start msedge --new-window --start-maximized https://bolt.dell.com/_layouts/15/FormServer.aspx?XmlLocation=/ServiceMgmt/%s%.xml
 
-  goto start
+echo https://bolt.dell.com/SiteAssets/v3/index.aspx/form/sm/%sm%
+start msedge --new-window --start-maximized https://bolt.dell.com/SiteAssets/v3/index.aspx/form/sm/%sm%
+
+goto start
